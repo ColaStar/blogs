b979aba107c9e93bb115c8e7ce38ac2e3f6edba2
吕鉴 <lvjian@lvjiandeMacBook-Pro.local>
2019-06-22 04:24:19 +0800

    blogs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,13 +21,7 @@ module.exports = {
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@public': '/docs/public'
-      }
-    }
-  },
+
   themeConfig: {
     sidebarDepth: 4, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间,
