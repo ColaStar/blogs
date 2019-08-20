65d442efdf3745d31f12db703f449cff36a8c7f7
lvjian1212 <928808423@qq.com>
2019-08-20 13:23:20 +0800

    webpack

---
+++ views/js/tj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ada0f7abc86e5ed3b0ee0c2e727998f4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
