dccc2d5194d0686334ef1166827c53eaf0512942
Cola <lvjianwy@yeah.net>
2019-08-28 23:12:29 +0800

    点击效果上传

--- docs/.vuepress/public/js/code.js
+++ docs/.vuepress/public/js/code.js
@@ -1,15 +1,17 @@
 // JavaScript Document
 $(document).ready(function () {
-    var bodyobj = document.querySelector("body"); 
-    var divobj = document.createElement("div"); 
-    divobj.innerHTML= "<canvas id='matrixBG' width='2560' height='200'></canvas>"
+    var bodyobj = document.querySelector("body");
+    var divobj = document.createElement("div");
+    divobj.innerHTML = "<canvas id='matrixBG' width='2560' height='200'></canvas>"
     bodyobj.appendChild(divobj);
     // document.write("");  
-    $('#matrixBG').css({"position": "fixed",
+    $('#matrixBG').css({
+        "position": "fixed",
         "bottom": 0,
         "left": 0,
         "z-index": -1,
-        "opacity": 0.07})
+        "opacity": 0.07
+    })
     var width = matrixBG.width = window.screen.width;
     var height = matrixBG.height = window.screen.height;
     var yPositions = Array(300).join(0).split('');
@@ -47,32 +49,31 @@ $(document).ready(function () {
     StopMatrix();});
     $("button#play").click(function(){RunMatrix();});
     */
-});
-/* 鼠标特效 */ 
-var a_idx = 0; 
-$(document).ready(function($) { 
-    $("body").click(function(e) { 
-        var a = new Array("难凉热血"); 
-        var $i = $("<span></span>").text(a[a_idx]); 
-        a_idx = (a_idx + 1) % a.length; 
-        var x = e.pageX, 
-        y = e.pageY; 
-        $i.css({ 
-            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999, 
-            "top": y - 20, 
-            "left": x, 
-            "position": "absolute", 
-            "font-weight": "bold", 
-            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+") "
-        }); 
-        $("body").append($i); 
-        $i.animate({ 
-            "top": y - 180, 
-            "opacity": 0 
-        }, 
-        1500, 
-        function() { 
-            $i.remove(); 
-        }); 
-    }); 
+
+    /* 鼠标特效 */
+    var a_idx = 0;
+    $("body").click(function (e) {
+        var a = new Array("难凉热血");
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+            y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ") "
+        });
+        $("body").append($i);
+        $i.animate({
+                "top": y - 180,
+                "opacity": 0
+            },
+            1500,
+            function () {
+                $i.remove();
+            });
+    });
 });
