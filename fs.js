8ad93e4b8dc4d0ac14e112cd5b3b40d512c3d81c
Cola <lvjianwy@yeah.net>
2019-09-11 23:28:03 +0800

    deploy

--- fs.js
+++ fs.js
@@ -1,7 +1,14 @@
 
 var fs = require('fs');
 var join = require('path').join;
- 
+let path = require('path')
+class generateDir {
+    constructor(repPath, outputFile, path) {
+      this.repPath = repPath
+      this.outputFile = outputFile
+      this.path = path
+    }
+}
 function getJsonFiles(jsonPath){
     let jsonFiles = [];
     function findJsonFile(path){
@@ -10,11 +17,12 @@ function getJsonFiles(jsonPath){
             let fPath = join(path,item);
             let stat = fs.statSync(fPath);
             
-            
             if(stat.isDirectory() === true) {
                 findJsonFile(fPath);
             }
             var reg = /.\md$/g
+            var pathList = fPath.split('/');
+            var directory = pathList[pathList.length-2]
             if (stat.isFile() === true&&reg.test(fPath) ) { 
               jsonFiles.push(fPath);
             }
