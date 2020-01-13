288e7781952e5ae11829f0fb4cf89c06b4c1230b
Cola <lvjianwy@yeah.net>
2020-01-13 13:20:11 +0800

    config

--- docs/other/手写各种/Untitled-2.js
+++ docs/other/手写各种/Untitled-2.js
@@ -1,26 +1,26 @@
-// console.log(1);
-// setTimeout(()=>{
-// console.log(2)
-// })
-// process.nextTick(()=>{
-// console.log(3)
-// })
-// setImmediate(()=>{
-// console.log(4)
-// })
-// new Promise(resolve=>{
-// console.log(5)
-// resolve()
-// console.log(6)
-// }).then(()=>{
-// console.log(7)
-// })
-// Promise.resolve().then(()=>{
-// console.log(8)
-// process.nextTick(()=>{
-// console.log(9)
-// })
-// })
+console.log(1);
+setTimeout(()=>{
+console.log(2)
+})
+process.nextTick(()=>{
+console.log(3)
+})
+setImmediate(()=>{
+console.log(4)
+})
+new Promise(resolve=>{
+console.log(5)
+resolve()
+console.log(6)
+}).then(()=>{
+console.log(7)
+})
+Promise.resolve().then(()=>{
+console.log(8)
+process.nextTick(()=>{
+console.log(9)
+})
+})
 
 
 var arr = [1,2,3,4,-1,-2,-3];
