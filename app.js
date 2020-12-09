366c41e9a2f52bc97fd15d4a588b85b642b80b8d
colastar <lvjianwy@yeah.net>
2020-12-09 15:13:40 +0800

    feat: 000

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app= express();
+
+app.use('/static', express.static(path.join(__dirname, 'views')))
+app.get('/', (req, res)=>{
+    res.send('Hello world');
+});
+app.listen(8083, ()=>{
+    console.log('Server is running at 8083 ,nb plus')
+})
